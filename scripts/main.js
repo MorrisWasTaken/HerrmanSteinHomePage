a255851c5bd1307aad6dbf7a23148c715c166810
Morris <MorrisEqualsDumb@gmail.com>
2025-08-14 13:24:48 +0200

    Added new Images and more - Added more images form the game - Removed RFE Banner - Made Interactive slideshow of images with javascript

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,28 @@
-// Store a reference to the <h1> in a variable
-const myHeading = document.querySelector("h1");
-// Update the text content of the <h1>
-myHeading.textContent = "Hello world!";
+const myImage = document.querySelector("img");
+var imgCounter = 0;
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (imgCounter == 0) {
+    myImage.setAttribute("src", "images/TWDIndevImages/OutsideDay.png");
+    imgCounter = 1;
+    } else if (imgCounter == 1) {
+        myImage.setAttribute("src", "images/TWDIndevImages/InsideDark.png");
+        imgCounter = 2;
+    } else if (imgCounter == 2) {
+        myImage.setAttribute("src", "images/TWDIndevImages/InsideLight.png");
+        imgCounter = 3;
+    } else if (imgCounter == 3) {
+        myImage.setAttribute("src", "images/TWDIndevImages/Shuttle.png");
+        imgCounter = 4;
+    } else if (imgCounter == 4) {
+    myImage.setAttribute("src", "images/TWDIndevImages/Space.png");
+    imgCounter = 5;
+    } else if (imgCounter == 5) {
+        myImage.setAttribute("src", "images/TWDIndevImages/Terminal.png");
+        imgCounter = 6;
+  } else {
+    myImage.setAttribute("src", "images/TWDIndevImages/OutsideNight.png");
+    imgCounter = 0;
+  }
+});
